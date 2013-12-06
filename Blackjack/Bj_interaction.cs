f066edb4b4ec96438c4b68060e7cabfd8a6fc5a8
ErikVincelette <multisol5@hotmail.com>
2013-12-06 12:14:59 +0100

    Added comment

--- Blackjack/Bj_interaction.cs
+++ Blackjack/Bj_interaction.cs
@@ -8,6 +8,8 @@ using System.Windows.Media.Imaging;
 
 namespace Blackjack
 {
+    // handles all interaction between main window and code-behind classes with data and logic
+      
     class Bj_interaction
     {
         public short bets_placed;
