84cd8e25bff46a9c164f843527b9903e215d5afa
Mickehb <michael.andersson3@comhem.se>
2013-12-06 13:56:44 +0100

    testar GitHub

--- Blackjack/Bj_interaction.cs
+++ Blackjack/Bj_interaction.cs
@@ -10,6 +10,8 @@ namespace Blackjack
 {
     class Bj_interaction
     {
+        // testing Github får facks säjk
+
         public short bets_placed;
         private Players players;
         public CardDeck deck;               // Warning Puclic
