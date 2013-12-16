14152fb3ce8f987abb3a8931a5da72e789c6991f
ErikVincelette <multisol5@hotmail.com>
2013-12-16 23:57:28 +0100

    added save window

---
+++ Blackjack/save_window.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Blackjack
+{
+    /// <summary>
+    /// Interaction logic for save_window.xaml
+    /// </summary>
+    public partial class save_window : Window
+    {
+        public ObservableCollection<string> saves { get; set; }
+        public save_window()
+        {
+            InitializeComponent();
+            saves = new ObservableCollection<string>();
+            save_list.DataContext = saves;
+            using (var db = new Blackjack_DBEntities1())
+            {
+                var query = from s in db.Saves_DB
+                            select s.save_name;
+                foreach (var item in query)
+                {
+                    saves.Add(item);
+                }
+
+            }
+
+        }
+
+        private void cancel_button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void save_button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            
+        }
+    }
+}
